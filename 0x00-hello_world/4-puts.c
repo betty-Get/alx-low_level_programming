67f8700fea9f11d406533e31980a49fc03375dc2
betty-Get <betelhem.getachew2926@gmail.com>
2022-03-12 11:25:45 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
+/**
+ * main - prints text using puts()
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
