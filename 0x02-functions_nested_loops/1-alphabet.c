aab9a3753577f68de31c288a9e2cdf1ff3267cfd
betty-Get <betelhem.getachew2926@gmail.com>
2022-03-16 11:04:52 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+
+ void print_alphabet(void){
+        for(char i='a'; i <= 'z'; i++){
+            putchar(i);
+            putchar('\n');
+        }
