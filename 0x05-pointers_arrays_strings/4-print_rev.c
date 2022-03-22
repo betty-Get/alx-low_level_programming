00a7a11a2102768a5189321207f222493bc42228
betty-Get <betelhem.getachew2926@gmail.com>
2022-03-22 06:18:43 -0700

    new

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - function with one argument
+ * @s: char type argument
+ *
+ * Description: reversing a string
+ * Return: na
+ */
+void print_rev(char *s)
+{
+	for(int i = strlen(s)-1; i>=0; i--)
+	{
+		putchar(s[i]);
+	}
+}
