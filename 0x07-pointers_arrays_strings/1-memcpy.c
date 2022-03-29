5ed1a009c6f13e8d5a49c4db236fadf75f4a1f00
betty-Get <betelhem.getachew2926@gmail.com>
2022-03-29 00:51:51 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * *_memcpy - copy data form src to dest
- * @dest: copy data to dest
- * @src: data copied from here
- * @n: copied to n memory space
+ * *_memcpy - copies memory area
+ * @dest: destination memory area
+ * @src: memory area to copy from
+ * @n: number of bytes to copy
  *
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	
-	for (i = 0; i <= n; i++)
+
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	return(dest);
+	return (dest);
 }
