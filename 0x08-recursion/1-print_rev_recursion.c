78cbc4921567277988bf7b66345f851c09135c7a
betty-Get <betelhem.getachew2926@gmail.com>
2022-03-30 07:22:59 -0700

    reverseString

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string, followed by a new line.
+ * _print_rev_recursion - Prints a string, followed by a new line.
  * @s: The string to be printed.
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
